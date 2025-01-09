0fe5f8cf9c00dca3897c84760806a89373ad84b0
Minh Vương <hardingadonis@gmail.com>
2025-01-09 16:25:48 +0700

    add: set up Husky hooks for linting and commit message validation

---
+++ commitlint.config.mjs
@@ -0,0 +1,10 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			['add', 'update', 'fix', 'docs', 'feat', 'refactor', 'delete'],
+		],
+	},
+};
