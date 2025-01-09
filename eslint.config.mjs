9070ca26302364cdba6643cb2f884f16c566df28
Minh Vương <hardingadonis@gmail.com>
2025-01-09 08:46:43 +0700

    add: upload base project for frontend

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(eslintPluginPrettierRecommended);
