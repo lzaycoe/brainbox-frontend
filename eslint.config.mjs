550b367a93bc75f64187c91242459f111ccd51e6
Minh Vương <hardingadonis@gmail.com>
2025-01-09 12:23:08 +0700

    add: integrate nuxt-security and eslint-plugin-vue for enhanced security and linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginVue from 'eslint-plugin-vue';
 
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(eslintPluginPrettierRecommended);
+export default withNuxt(eslintPluginPrettierRecommended, pluginVue);
