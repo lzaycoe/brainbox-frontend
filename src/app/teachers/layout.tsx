caf4ee6a4bafb134115afe55d85e9a4054c501fe
Quốc Chương <quocchuong2609@gmail.com>
2025-01-21 14:07:54 +0700

    add: layout for teacher

---
+++ src/app/teachers/layout.tsx
@@ -0,0 +1,56 @@
+/*
+ *  ======================================================================
+ *  Copyright (C) 2025 - lzaycoe (Lazy Code)
+ *  ======================================================================
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  ======================================================================
+ */
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import '@/app/globals.css';
+import TeacherLayout from '@/layouts/TeacherLayout';
+
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+	title: 'BrainBox | Teacher Dashboard',
+	description: 'An e-learning platform built with Next.js and Shadcn UI',
+};
+
+export default function TeacherPageLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen`}
+			>
+				<TeacherLayout>{children}</TeacherLayout>
+			</body>
+		</html>
+	);
+}
