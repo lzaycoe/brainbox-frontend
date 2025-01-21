42a563fb3f2257a7c2c6dbf5c32ef3593122a885
Quốc Chương <quocchuong2609@gmail.com>
2025-01-21 09:56:12 +0700

    add: teacher layout

---
+++ src/layouts/TeacherLayout.tsx
@@ -0,0 +1,35 @@
+/*
+ *  ======================================================================
+ *  Copyright (C) 2025 - lzaycoe (Lazy Code)
+ *  ======================================================================
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  ======================================================================
+ */
+import React from 'react';
+
+import { Sidebar } from '@/components/commons/teachers/SideBar';
+
+const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
+	const a = 5;
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Sidebar />
+			<main className="flex-grow">{children}</main>
+		</div>
+	);
+};
+
+export default TeacherLayout;
