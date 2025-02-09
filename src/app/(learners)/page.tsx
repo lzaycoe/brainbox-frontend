e42bc643f6a0f97630444e6f3af031d7c66bf2c2
Thunder <htn10a2@gmail.com>
2025-02-09 19:07:12 +0700

    update: add component list card teacher to page

--- src/app/(learners)/page.tsx
+++ src/app/(learners)/page.tsx
@@ -23,6 +23,7 @@ import BecomeTeacherSteps from '@/components/home/BecomeTeacherSteps';
 import { BrowseCategories } from '@/components/home/BrowseCategories';
 import ListCourseCard from '@/components/home/ListCourseCard';
 import ListCourseCardLandscape from '@/components/home/ListCourseCardLandscape';
+import ListTeacherCard from '@/components/home/ListTeacherCard';
 
 export default function Home() {
 	return (
@@ -32,6 +33,7 @@ export default function Home() {
 			<ListCourseCard />
 			<ListCourseCardLandscape />
 			<BecomeTeacherSteps />
+			<ListTeacherCard />
 		</div>
 	);
 }
