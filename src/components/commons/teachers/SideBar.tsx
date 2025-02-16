bdc9295c2ef57d066b602c3110c758461f254a2b
Thunder <htn10a2@gmail.com>
2025-02-16 17:40:28 +0700

    update: effect when navigate for sidebar

--- src/components/commons/teachers/SideBar.tsx
+++ src/components/commons/teachers/SideBar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import {
 	PiChartBarLight,
 	PiChatCircleDots,
@@ -10,6 +11,14 @@ import {
 } from 'react-icons/pi';
 
 export const SideBar = () => {
+	const pathname = usePathname();
+
+	const getLinkClasses = (path: string) => {
+		return pathname === path
+			? 'flex items-center gap-3 px-6 py-3 bg-orange-500 text-white'
+			: 'flex items-center gap-3 px-6 py-3 text-gray-400 hover:text-white';
+	};
+
 	return (
 		<div className="flex flex-col bg-neutral-800 text-white shadow-sm sm:w-[300px] fixed top-0 left-0 h-full">
 			<div className="px-6 py-5 text-2xl font-semibold">
@@ -27,37 +36,31 @@ export const SideBar = () => {
 			</div>
 			<nav className="flex flex-col mt-4 space-y-2">
 				<Link
-					href="#dashboard"
-					className="flex items-center gap-3 px-6 py-3 bg-orange-500 text-white"
+					href="/teachers/dashboard"
+					className={getLinkClasses('/teachers/dashboard')}
 				>
 					<PiChartBarLight className="w-8 h-8" />
 					Dashboard
 				</Link>
 				<Link
-					href="#create-course"
-					className="flex items-center gap-3 px-6 py-3 text-gray-400 hover:text-white"
+					href="/create-course"
+					className={getLinkClasses('/create-course')}
 				>
 					<PiPlusCircleBold className="w-8 h-8" />
 					Create New Course
 				</Link>
 				<Link
-					href="#my-courses"
-					className="flex items-center gap-3 px-6 py-3 text-gray-400 hover:text-white"
+					href="/teachers/courses"
+					className={getLinkClasses('/teachers/courses')}
 				>
 					<PiStack className="w-8 h-8" />
 					My Courses
 				</Link>
-				<Link
-					href="#earnings"
-					className="flex items-center gap-3 px-6 py-3 text-gray-400 hover:text-white"
-				>
+				<Link href="/earnings" className={getLinkClasses('/earnings')}>
 					<PiCreditCard className="w-8 h-8" />
 					Earnings
 				</Link>
-				<Link
-					href="#messages"
-					className="flex items-center justify-between px-6 py-3 text-gray-400 hover:text-white"
-				>
+				<Link href="/messages" className={getLinkClasses('/messages')}>
 					<div className="flex items-center gap-3">
 						<PiChatCircleDots className="w-8 h-8" />
 						Messages
@@ -67,8 +70,8 @@ export const SideBar = () => {
 					</div>
 				</Link>
 				<Link
-					href="#settings"
-					className="flex items-center gap-3 px-6 py-3 text-gray-400 hover:text-white"
+					href="/teachers/settings"
+					className={getLinkClasses('/teachers/settings')}
 				>
 					<PiGear className="w-8 h-8" />
 					Settings
