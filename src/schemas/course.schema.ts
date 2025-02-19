0d7702159bd55435e381698cc6f386ed5fbc9285
Thunder <htn10a2@gmail.com>
2025-02-19 19:27:28 +0700

    add: course schema
    Use for form

---
+++ src/schemas/course.schema.ts
@@ -0,0 +1,23 @@
+import * as z from 'zod';
+
+export const courseSchema = z.object({
+	title: z
+		.string()
+		.nonempty({ message: 'Title is required' })
+		.max(80, { message: 'Title must be less than 80 characters' }),
+	subtitle: z
+		.string()
+		.max(120, { message: 'Subtitle must be less than 120 characters' }),
+	tag: z.string().nonempty({ message: 'Category is required' }),
+	description: z.string().nonempty({ message: 'Description is required' }),
+	thumbnail: z.string().nonempty({ message: 'Thumbnail is required' }),
+	originPrice: z
+		.number()
+		.min(0, { message: 'Original price must be a positive number' }),
+	salePrice: z
+		.number()
+		.min(0, { message: 'Sale price must be a positive number' }),
+	public: z.boolean().optional().default(false),
+});
+
+export type FormData = z.infer<typeof courseSchema>;
