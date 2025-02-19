0e8e8a2ed362f1065a2847543b9bec480daf6e87
Yuht <nguyenthithuy1022003@gmail.com>
2025-02-20 01:31:20 +0700

    update: update UI

--- src/components/learners/purchase-history/Content.tsx
+++ src/components/learners/purchase-history/Content.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { FaArrowDownLong } from 'react-icons/fa6';
 
 import ContentCard from './ContentCard';
 import CourseItem from './CourseItem';
 
-// Dữ liệu mẫu
 const orderData = {
 	date: '12th January, 2025 at 11:30 PM',
 	courses: 2,
@@ -42,8 +42,8 @@ interface SummaryProps {
 }
 
 const Summary: React.FC<SummaryProps> = ({ user, cardNumber, expiryDate }) => (
-	<aside className="bg-white shadow-sm w-full max-w-[450px] p-6 text-black flex-shrink-0 flex flex-col justify-center h-full">
-		{/* Đảm bảo ContentCard cũng được căn trái */}
+	<aside className="bg-white w-full max-w-[500px] p-6 text-black flex-shrink-0 flex flex-col relative">
+		<div className="absolute top-0 left-0 w-[1px] h-[calc(100%+40px)] bg-gray-300"></div>
 		<div className="text-left">
 			<ContentCard
 				date={orderData.date}
@@ -53,11 +53,10 @@ const Summary: React.FC<SummaryProps> = ({ user, cardNumber, expiryDate }) => (
 			/>
 		</div>
 
-		{/* Thông tin thêm của Summary */}
 		<div className="mt-4 ml-7 flex items-center text-sm gap-7 text-left">
 			<span>{user}</span>
 			<span>{cardNumber}</span>
-			<span>{expiryDate}</span>
+			<span className="ml-14">{expiryDate}</span>
 		</div>
 	</aside>
 );
@@ -65,19 +64,25 @@ const Summary: React.FC<SummaryProps> = ({ user, cardNumber, expiryDate }) => (
 // Main Component
 const Content = () => {
 	return (
-		<article className="flex flex-col items-center py-6 bg-white border border-solid shadow-lg border-[#E9EAF0]">
-			<section className="flex flex-wrap gap-5 justify-between w-full max-w-[1272px]">
+		<article className="flex flex-col items-center py-6 bg-white border border-solid shadow-lg border-[#E9EAF0] relative">
+			{/* Icon mũi tên */}
+			<div className="flex items-center justify-between w-full relative">
 				<ContentCard
 					date={orderData.date}
 					courses={orderData.courses}
 					totalPrice={orderData.totalPrice}
 					paymentMethod={orderData.paymentMethod}
 				/>
-			</section>
+
+				{/* Icon mũi tên */}
+				<div className="flex items-center justify-center w-12 h-12 bg-[#F5F7FA] rounded-md shadow-md border border-gray-300 mr-5">
+					<FaArrowDownLong className="text-[#1D2026] text-2xl" />
+				</div>
+			</div>
 
 			<hr className="self-stretch mt-6 w-full bg-gray-200 border-0 h-px" />
 
-			<section className="flex gap-10 justify-between mt-6 w-full max-w-[1272px]">
+			<section className="flex gap-10 justify-between items-center w-full max-w-[1272px]">
 				<div className="w-full max-w-[calc(100%-350px-40px)]">
 					{orderData.items.map((course) => (
 						<CourseItem key={course.id} course={course} />
