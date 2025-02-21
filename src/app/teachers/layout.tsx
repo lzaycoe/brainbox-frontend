6c082e8f92372f80f6bbfae94f08292f57080a92
Thunder <htn10a2@gmail.com>
2025-02-21 19:48:08 +0700

    update: import toaster into layout
    For display toast

--- src/app/teachers/layout.tsx
+++ src/app/teachers/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import '@/app/globals.css';
+import { Toaster } from '@/components/ui/toaster';
 import TeacherLayout from '@/layouts/TeacherLayout';
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function TeacherPageLayout({
 				className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen`}
 			>
 				<TeacherLayout>{children}</TeacherLayout>
+				<Toaster />
 			</div>
 		</div>
 	);
