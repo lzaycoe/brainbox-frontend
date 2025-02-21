725c9c1a8fa940c26d756a5e26d6365e6439c0d0
Thunder <htn10a2@gmail.com>
2025-02-21 19:52:34 +0700

    add: create section dialog

---
+++ src/components/teachers/create-section/CreateSectionDialog.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog';
+import { useToast } from '@/hooks/use-toast';
+import { SectionData, sectionSchema } from '@/schemas/section.schema';
+import { createSection } from '@/services/api/section';
+
+export default function CreateSectionDialog({
+	courseId,
+}: {
+	courseId: string;
+}) {
+	const router = useRouter();
+	const { toast } = useToast();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<SectionData>({
+		resolver: zodResolver(sectionSchema),
+	});
+
+	const onSubmit = async (data: SectionData) => {
+		try {
+			await createSection(courseId, data);
+			toast({
+				title: 'Success',
+				description: 'Section created successfully!',
+			});
+			router.refresh();
+		} catch (error) {
+			toast({
+				title: 'Error',
+				description: 'Failed to create section.',
+				variant: 'destructive',
+			});
+			console.error('Failed to create section:', error);
+		}
+	};
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button className="bg-[#ffeee8] text-[#ff6636] text-base font-semibold capitalize leading-[48px] hover:bg-[#ff6636] hover:text-white">
+					Add Sections
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Section Name</DialogTitle>
+				</DialogHeader>
+				<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+					<div className="flex flex-col space-y-1">
+						<label
+							htmlFor="sectionName"
+							className="text-sm font-medium text-[#1d1f26]"
+						>
+							Section
+						</label>
+						<input
+							id="title"
+							{...register('title')}
+							className="w-full pl-4 pr-4 pt-3 pb-3 bg-white border border-[#e8eaef] text-base font-normal text-[#8c93a3]"
+							placeholder="Write your section name here.."
+						/>
+						{errors.title && (
+							<p className="text-red-500 text-sm">{errors.title.message}</p>
+						)}
+					</div>
+					<div className="flex justify-between">
+						<DialogClose asChild>
+							<Button
+								type="button"
+								variant="outline"
+								className="bg-[#f4f7f9] text-[#1d1f26]"
+							>
+								Cancel
+							</Button>
+						</DialogClose>
+						<Button type="submit" className="bg-[#ff6636] text-white">
+							Save
+						</Button>
+					</div>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+}
