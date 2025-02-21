291f160c288e168242c17fe194dfd6a2e4fb432b
Thunder <htn10a2@gmail.com>
2025-02-21 19:50:09 +0700

    add: section schema

---
+++ src/schemas/section.schema.ts
@@ -0,0 +1,12 @@
+import * as z from 'zod';
+
+export const sectionSchema = z.object({
+	title: z.string().nonempty({ message: 'Section name is required' }),
+});
+
+export type SectionData = z.infer<typeof sectionSchema>;
+
+export interface Section {
+	id: number;
+	title: string;
+}
