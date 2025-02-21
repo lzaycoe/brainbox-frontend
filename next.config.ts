205be4ead2fc7b92bdffaf5727f18e92648811cd
Quốc Chương <quocchuong2609@gmail.com>
2025-02-21 11:02:49 +0700

    add: allow images from cdn.builder.io in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
 				hostname: 'img.clerk.com',
 				pathname: '/**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.builder.io',
+				pathname: '/**',
+			},
 		],
 		dangerouslyAllowSVG: true,
 	},
