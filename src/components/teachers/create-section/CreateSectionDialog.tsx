863c88e7ba0243b098f40e4dce29bd669f367b0b
Thunder <htn10a2@gmail.com>
2025-02-25 20:14:20 +0700

    fix: fix duplicate code

--- src/components/teachers/create-section/CreateSectionDialog.tsx
+++ src/components/teachers/create-section/CreateSectionDialog.tsx
@@ -1,22 +1,21 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
 
 import { Button } from '@/components/ui/button';
 import {
 	Dialog,
-	DialogClose,
 	DialogContent,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
 import { useToast } from '@/hooks/use-toast';
-import { Section, SectionData, sectionSchema } from '@/schemas/section.schema';
+import { Section, SectionData } from '@/schemas/section.schema';
 import { createSection } from '@/services/api/section';
+
+import SectionForm from './SectionForm';
 
 export default function CreateSectionDialog({
 	courseId,
@@ -28,15 +27,8 @@ export default function CreateSectionDialog({
 	const router = useRouter();
 	const { toast } = useToast();
 	const [isOpen, setIsOpen] = useState(false);
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<SectionData>({
-		resolver: zodResolver(sectionSchema),
-	});
 
-	const onSubmit = async (data: SectionData) => {
+	const handleCreate = async (data: SectionData) => {
 		try {
 			const newSection = await createSection(courseId, data);
 			toast({
@@ -68,39 +60,7 @@ export default function CreateSectionDialog({
 				<DialogHeader>
 					<DialogTitle>Section Name</DialogTitle>
 				</DialogHeader>
-				<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-					<div className="flex flex-col space-y-1">
-						<label
-							htmlFor="sectionName"
-							className="text-sm font-medium text-[#1d1f26]"
-						>
-							Section
-						</label>
-						<input
-							id="title"
-							{...register('title')}
-							className="w-full pl-4 pr-4 pt-3 pb-3 bg-white border border-[#e8eaef] text-base font-normal text-[#8c93a3]"
-							placeholder="Write your section name here.."
-						/>
-						{errors.title && (
-							<p className="text-red-500 text-sm">{errors.title.message}</p>
-						)}
-					</div>
-					<div className="flex justify-between">
-						<DialogClose asChild>
-							<Button
-								type="button"
-								variant="outline"
-								className="bg-[#f4f7f9] text-[#1d1f26]"
-							>
-								Cancel
-							</Button>
-						</DialogClose>
-						<Button type="submit" className="bg-[#ff6636] text-white">
-							Save
-						</Button>
-					</div>
-				</form>
+				<SectionForm onSubmit={handleCreate} buttonText="Save" />
 			</DialogContent>
 		</Dialog>
 	);
--- src/components/teachers/create-section/UpdateSectionDialog.tsx
+++ src/components/teachers/create-section/UpdateSectionDialog.tsx
@@ -1,23 +1,22 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import { PiPencilSimpleLineBold } from 'react-icons/pi';
 
 import { Button } from '@/components/ui/button';
 import {
 	Dialog,
-	DialogClose,
 	DialogContent,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
 import { useToast } from '@/hooks/use-toast';
-import { Section, SectionData, sectionSchema } from '@/schemas/section.schema';
+import { Section, SectionData } from '@/schemas/section.schema';
 import { updateSection } from '@/services/api/section';
+
+import SectionForm from './SectionForm';
 
 interface UpdateSectionDialogProps {
 	courseId: string;
@@ -35,16 +34,8 @@ const UpdateSectionDialog: React.FC<UpdateSectionDialogProps> = ({
 	const router = useRouter();
 	const { toast } = useToast();
 	const [isOpen, setIsOpen] = useState(false);
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<SectionData>({
-		resolver: zodResolver(sectionSchema),
-		defaultValues: { title: initialTitle },
-	});
 
-	const onSubmit = async (data: SectionData) => {
+	const handleUpdate = async (data: SectionData) => {
 		try {
 			const updatedSection = await updateSection(courseId, sectionId, data);
 			toast({
@@ -76,39 +67,11 @@ const UpdateSectionDialog: React.FC<UpdateSectionDialogProps> = ({
 				<DialogHeader>
 					<DialogTitle>Update Section Name</DialogTitle>
 				</DialogHeader>
-				<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-					<div className="flex flex-col space-y-1">
-						<label
-							htmlFor="sectionName"
-							className="text-sm font-medium text-[#1d1f26]"
-						>
-							Section
-						</label>
-						<input
-							id="title"
-							{...register('title')}
-							className="w-full pl-4 pr-4 pt-3 pb-3 bg-white border border-[#e8eaef] text-base font-normal text-[#8c93a3]"
-							placeholder="Write your section name here.."
-						/>
-						{errors.title && (
-							<p className="text-red-500 text-sm">{errors.title.message}</p>
-						)}
-					</div>
-					<div className="flex justify-between">
-						<DialogClose asChild>
-							<Button
-								type="button"
-								variant="outline"
-								className="bg-[#f4f7f9] text-[#1d1f26]"
-							>
-								Cancel
-							</Button>
-						</DialogClose>
-						<Button type="submit" className="bg-[#ff6636] text-white">
-							Save
-						</Button>
-					</div>
-				</form>
+				<SectionForm
+					initialTitle={initialTitle}
+					onSubmit={handleUpdate}
+					buttonText="Save"
+				/>
 			</DialogContent>
 		</Dialog>
 	);
