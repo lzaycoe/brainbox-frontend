d66125638149a8e3c835f017cbe6ff40a77b5930
linhlm0210 <lammylinh7012@gmail.com>
2025-02-26 22:49:16 +0700

    add: page

---
+++ src/app/(learners)/course-list/page.tsx
@@ -0,0 +1,9 @@
+import Course from '@/components/learners/course-list/Course';
+
+export default function CourseList() {
+	return (
+		<div className="flex flex-col justify-center items-center px-4 mt-10 mb-10">
+			<Course />
+		</div>
+	);
+}
