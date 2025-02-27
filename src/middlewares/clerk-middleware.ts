bc8511d82e7bb980ee1a73a1858368e64f9f12dc
Thunder <htn10a2@gmail.com>
2025-02-27 22:17:41 +0700

    update: clerk middle ware to handle route

--- src/middlewares/clerk-middleware.ts
+++ src/middlewares/clerk-middleware.ts
@@ -1,6 +1,35 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkClient } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+	'/teachers(.*)',
+	'/courses(.*)',
+	'/settings(.*)',
+	'/dashboard(.*)',
+	'/watch-course(.*)',
+	'/whishlist(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+	const { userId } = await auth();
+
+	if (isProtectedRoute(req) && !userId) {
+		return (await auth()).redirectToSignIn({ returnBackUrl: req.url });
+	}
+
+	let role: 'learner' | 'teacher' | undefined;
+	if (userId) {
+		const user = await (await clerkClient()).users.getUser(userId);
+		role = user.publicMetadata.role as 'learner' | 'teacher' | undefined;
+	}
+
+	if (req.nextUrl.pathname.startsWith('/teachers') && role !== 'teacher') {
+		return NextResponse.redirect(new URL('/become-instructor', req.url));
+	}
+
+	return NextResponse.next();
+});
 
 export const config = {
 	matcher: [
