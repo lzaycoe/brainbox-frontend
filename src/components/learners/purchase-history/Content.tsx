e2e9f0c6f3d11bc332910ceff4c6de5deeb82b5a
linhlm0210 <lammylinh7012@gmail.com>
2025-03-03 21:27:50 +0700

    add: filter

--- src/components/learners/purchase-history/Content.tsx
+++ src/components/learners/purchase-history/Content.tsx
@@ -4,10 +4,14 @@ import { useUser } from '@clerk/nextjs';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
+import PaginationCustom from '@/components/commons/PaginationCustom';
 import { User } from '@/schemas/user.schema';
 import { getCourse } from '@/services/api/course';
 import { getPaymentsByUserId } from '@/services/api/payment';
 import { getUserByClerkId, getUserClerk } from '@/services/api/user';
+import { formatPrice } from '@/utils/formatPrice';
+
+import FilterStatusSelects from './FilterStatusSelects';
 
 interface Payment {
 	id: number;
@@ -29,11 +33,33 @@ interface Course {
 }
 
 const PaymentList = () => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const paymentsPerPage = 5;
 	const [payments, setPayments] = useState<Payment[]>([]);
 	const [userData, setUserData] = useState<User | null>(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
 	const { user } = useUser();
+	const [selectedStatus, setSelectedStatus] = useState('all');
+
+	const indexOfLastPayment = currentPage * paymentsPerPage;
+	const indexOfFirstPayment = indexOfLastPayment - paymentsPerPage;
+
+	const filteredPayments = payments.filter((payment) => {
+		const matchStatus =
+			selectedStatus === 'all' ||
+			payment.status.toLowerCase() === selectedStatus.toLowerCase();
+		return matchStatus;
+	});
+
+	const currentPayment = filteredPayments.slice(
+		indexOfFirstPayment,
+		indexOfLastPayment,
+	);
+
+	const totalPages = Math.ceil(filteredPayments.length / paymentsPerPage);
+
+	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	const fetchUser = async () => {
 		try {
@@ -81,7 +107,9 @@ const PaymentList = () => {
 				}),
 			);
 
-			setPayments(paymentsWithCourses);
+			const sortedPayments = paymentsWithCourses.sort((a, b) => b.id - a.id);
+
+			setPayments(sortedPayments);
 		} catch (err) {
 			console.error(err);
 			setError('Could not fetch payments. Please try again later.');
@@ -142,59 +170,81 @@ const PaymentList = () => {
 
 	return (
 		<section className="w-full mt-10 space-y-6">
-			{payments.map((payment) => (
-				<div
-					key={payment.id}
-					className="border border-gray-300 shadow-md p-2 bg-white "
-				>
-					<article className="grid grid-cols-[55%_25%_20%] items-center max-w-full">
-						{payment.courseDetails ? (
-							<CourseItem course={payment.courseDetails} />
-						) : (
-							<section className="flex gap-5 items-start">
-								<Image
-									src="/app/become_a_teacher/become_a_teacher_1.png"
-									alt="Become a Teacher"
-									className="object-contain"
-									width={160}
-									height={90}
-								/>
-								<div className="flex flex-col justify-between min-h-[160px] w-full">
-									<div>
-										<h3 className="mt-2 text-lg text-black">
-											Become a Teacher
-										</h3>
-									</div>
-								</div>
-							</section>
-						)}
+			<FilterStatusSelects onStatusChange={setSelectedStatus} />
 
-						<div className="text-left">
-							<span className="text-orange-500 text-xl font-medium">
-								{payment.courseDetails?.salePrice || payment.price} VND
-							</span>
-							{payment.courseDetails?.originPrice && (
-								<span className="text-lg line-through text-gray-400 ml-2">
-									{payment.courseDetails.originPrice} VND
-								</span>
-							)}
-						</div>
-
-						<div className="flex justify-center items-center gap-2">
-							<span className="text-gray-500 text-sm font-medium">Status:</span>
-							<span
-								className={`px-2 py-1 rounded text-sm font-semibold ${
-									payment.status === 'paid'
-										? 'bg-green-100 text-green-600'
-										: 'bg-red-100 text-red-600'
-								}`}
-							>
-								{payment.status.toUpperCase()}
-							</span>
-						</div>
-					</article>
+			{filteredPayments.length === 0 ? (
+				<div className="col-span-4 mt-10 text-xl text-gray-500">
+					No payment found for your search.
 				</div>
-			))}
+			) : (
+				currentPayment.map((payment) => (
+					<div
+						key={payment.id}
+						className="border border-gray-300 shadow-md p-2 bg-white "
+					>
+						<article className="grid grid-cols-[55%_25%_20%] items-center max-w-full">
+							{payment.courseDetails ? (
+								<CourseItem course={payment.courseDetails} />
+							) : (
+								<section className="flex gap-5 items-start">
+									<Image
+										src="/app/become_a_teacher/become_a_teacher_1.png"
+										alt="Become a Teacher"
+										className="object-contain"
+										width={160}
+										height={90}
+									/>
+									<div className="flex flex-col justify-between min-h-[160px] w-full">
+										<div>
+											<h3 className="mt-2 text-lg text-black">
+												Become a Teacher
+											</h3>
+										</div>
+									</div>
+								</section>
+							)}
+
+							<div className="text-left">
+								<span className="text-orange-500 text-xl font-medium">
+									{formatPrice(
+										payment.courseDetails?.salePrice || payment.price,
+									)}
+								</span>
+								{payment.courseDetails?.originPrice && (
+									<span className="text-lg line-through text-gray-400 ml-2">
+										{formatPrice(payment.courseDetails.originPrice)}
+									</span>
+								)}
+							</div>
+
+							<div className="flex justify-center items-center gap-2">
+								<span className="text-gray-500 text-sm font-medium">
+									Status:
+								</span>
+								<span
+									className={`px-2 py-1 rounded text-sm font-semibold ${
+										payment.status === 'paid'
+											? 'bg-green-100 text-green-600'
+											: 'bg-red-100 text-red-600'
+									}`}
+								>
+									{payment.status.toUpperCase()}
+								</span>
+							</div>
+						</article>
+					</div>
+				))
+			)}
+
+			{filteredPayments.length > 0 && (
+				<PaginationCustom
+					currentPage={currentPage}
+					totalPages={totalPages}
+					onPageChange={paginate}
+					activeClassName="bg-[#FF6636] text-white"
+					hoverClassName="hover:bg-[#FFEEE8] hover:text-[#FF6636]"
+				/>
+			)}
 		</section>
 	);
 };
