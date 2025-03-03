a07a6f8dff869216bbc0b41fdf19efc7aee0e2ee
Bun He <hungoffsize@gmail.com>
2025-03-03 16:18:29 +0700

    add: use api to display card

--- src/components/learners/courses/CourseList.tsx
+++ src/components/learners/courses/CourseList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PaginationCustom from '@/components/commons/PaginationCustom';
 import SearchAndFilter from '@/components/commons/SearchAndFilter';
@@ -12,147 +12,42 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+import {
+	CourseData,
+	fetchPaidCoursesProgress,
+} from '@/services/custom/course/checkProgress';
 
-const coursesData = [
-	{
-		id: 1,
-		title: 'Reiki Level I, II and Master/Teacher Program',
-		thumbnail: '/app/course/course01.png',
-		completed: '0%',
-	},
-	{
-		id: 2,
-		title: 'Introduction to Python Programming',
-		thumbnail: '/app/course/course01.png',
-		completed: '20%',
-	},
-	{
-		id: 3,
-		title: 'Advanced JavaScript Concepts',
-		thumbnail: '/app/course/course01.png',
-		completed: '50%',
-	},
-	{
-		id: 4,
-		title: 'React for Beginners',
-		thumbnail: '/app/course/course01.png',
-		completed: '75%',
-	},
-	{
-		id: 5,
-		title: 'Mastering CSS Grid and Flexbox',
-		thumbnail: '/app/course/course01.png',
-		completed: '100%',
-	},
-	{
-		id: 6,
-		title: 'Node.js and Express.js Fundamentals',
-		thumbnail: '/app/course/course01.png',
-		completed: '30%',
-	},
-	{
-		id: 7,
-		title: 'Building RESTful APIs with Django',
-		thumbnail: '/app/course/course01.png',
-		completed: '60%',
-	},
-	{
-		id: 8,
-		title: 'Introduction to Machine Learning',
-		thumbnail: '/app/course/course01.png',
-		completed: '40%',
-	},
-	{
-		id: 9,
-		title: 'Data Structures and Algorithms',
-		thumbnail: '/app/course/course01.png',
-		completed: '90%',
-	},
-	{
-		id: 10,
-		title: 'Full-Stack Web Development',
-		thumbnail: '/app/course/course01.png',
-		completed: '10%',
-	},
-	{
-		id: 11,
-		title: 'Digital Marketing Strategies',
-		thumbnail: '/app/course/course01.png',
-		completed: '80%',
-	},
-	{
-		id: 12,
-		title: 'Graphic Design with Adobe Illustrator',
-		thumbnail: '/app/course/course01.png',
-		completed: '25%',
-	},
-	{
-		id: 13,
-		title: 'Photography Masterclass',
-		thumbnail: '/app/course/course01.png',
-		completed: '55%',
-	},
-	{
-		id: 14,
-		title: 'Music Production with Ableton Live',
-		thumbnail: '/app/course/course01.png',
-		completed: '70%',
-	},
-	{
-		id: 15,
-		title: 'Creative Writing Workshop',
-		thumbnail: '/app/course/course01.png',
-		completed: '35%',
-	},
-	{
-		id: 16,
-		title: 'Public Speaking and Presentation Skills',
-		thumbnail: '/app/course/course01.png',
-		completed: '45%',
-	},
-	{
-		id: 17,
-		title: 'Financial Analysis and Modeling',
-		thumbnail: '/app/course/course01.png',
-		completed: '65%',
-	},
-	{
-		id: 18,
-		title: 'Introduction to Blockchain Technology',
-		thumbnail: '/app/course/course01.png',
-		completed: '85%',
-	},
-	{
-		id: 19,
-		title: 'Cybersecurity Essentials',
-		thumbnail: '/app/course/course01.png',
-		completed: '95%',
-	},
-	{
-		id: 20,
-		title: 'Artificial Intelligence for Everyone',
-		thumbnail: '/app/course/course01.png',
-		completed: '50%',
-	},
-];
-
-const CourseList: React.FC = () => {
+const CourseList: React.FC<{ userId: number }> = ({ userId }) => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [searchQuery, setSearchQuery] = useState('');
 	const [statusFilter, setStatusFilter] = useState('all');
+	const [courses, setCourses] = useState<CourseData[]>([]);
 	const coursesPerPage = 12;
+
+	useEffect(() => {
+		const loadCourses = async () => {
+			const fetchedCourses = await fetchPaidCoursesProgress(userId);
+			if (fetchedCourses) {
+				setCourses(fetchedCourses);
+			} else {
+				setCourses([]);
+			}
+		};
+
+		loadCourses();
+	}, [userId]);
 
 	const indexOfLastCourse = currentPage * coursesPerPage;
 	const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
 
-	const filteredCourses = coursesData.filter((course) => {
+	const filteredCourses = courses.filter((course) => {
 		const matchesSearchQuery = course.title
 			.toLowerCase()
 			.includes(searchQuery.toLowerCase());
 		const matchesStatusFilter =
 			statusFilter === 'all' ||
-			(statusFilter === 'completed' && course.completed === '100%') ||
-			(statusFilter === 'in-progress' && course.completed !== '100%');
+			(statusFilter === 'completed' && course.completed === '100.0%') ||
+			(statusFilter === 'in-progress' && course.completed !== '100.0%');
 		return matchesSearchQuery && matchesStatusFilter;
 	});
 
@@ -160,7 +55,6 @@ const CourseList: React.FC = () => {
 		indexOfFirstCourse,
 		indexOfLastCourse,
 	);
-
 	const totalPages = Math.ceil(filteredCourses.length / coursesPerPage);
 
 	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
