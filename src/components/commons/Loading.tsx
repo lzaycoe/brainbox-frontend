daff7dde15ad2b59d4b8bd6d28b4dcdd48491739
Thunder <htn10a2@gmail.com>
2025-03-04 13:56:01 +0700

    update: style for loading component

--- src/components/commons/Loading.tsx
+++ src/components/commons/Loading.tsx
@@ -1,9 +1,15 @@
-// 'use client';
+'use client';
+
+import React from 'react';
 
 const Loading: React.FC = () => {
 	return (
-		<div className="flex justify-center items-center min-h-screen">
-			<div className="animate-spin rounded-full h-14 w-14 border-b-4 border-orange-500"></div>
+		<div className="flex flex-col justify-center items-center">
+			<div className="animate-spin rounded-full h-14 w-14 border-b-4 border-orange-500 mb-4"></div>
+			<div className="text-orange-500 text-lg font-semibold">
+				Loading
+				<span className="animate-dots">...</span>
+			</div>
 		</div>
 	);
 };
