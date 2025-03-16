6e87c5f9586afa951f758ca427886f1ed7117c3c
Bun He <hungoffsize@gmail.com>
2025-03-17 01:42:34 +0700

    fix: fix sonar scan

--- src/components/learners/category/ListCourseByCategory.tsx
+++ src/components/learners/category/ListCourseByCategory.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
-import CourseCard from '@/components/commons/CourseCard';
-import Loading from '@/components/commons/Loading';
-import { getCategoryColors } from '@/config/categoryColors';
-import { Course } from '@/schemas/course.schema';
+import CourseComponent from '@/components/learners/course-list/Course';
+import { type Course as BaseCourse } from '@/schemas/course.schema';
 import { getCourses } from '@/services/api/course';
 import { fetchPaidStudentsCount } from '@/services/api/payment';
 
+// Tạo interface Course phù hợp với CourseComponent
+interface Course extends BaseCourse {
+	id: number;
+	status: string;
+	teacherId: number;
+	createdAt: string;
+	updatedAt: string;
+	rating?: string;
+}
+
 const ListCourseByCategory: React.FC = () => {
-	const router = useRouter();
 	const [courses, setCourses] = useState<Course[]>([]);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
@@ -32,6 +38,9 @@ const ListCourseByCategory: React.FC = () => {
 							...course,
 							id: course.id || index + 1,
 							students,
+							createdAt: course.createdAt || new Date().toISOString(),
+							updatedAt: course.updatedAt || new Date().toISOString(),
+							rating: '0.0',
 						};
 					}),
 				);
@@ -47,38 +56,12 @@ const ListCourseByCategory: React.FC = () => {
 		fetchCourses();
 	}, []);
 
-	const handleCourseClick = (id: number) => {
-		router.push(`/courses/${id}`);
-	};
-
 	if (error) return <p className="text-red-500">{error}</p>;
 
 	return (
 		<section className="flex flex-col items-center py-10">
 			<div className="w-full max-w-7xl px-4">
-				{loading ? (
-					<Loading />
-				) : (
-					<div className="grid grid-cols-4 gap-6 max-md:grid-cols-2 max-sm:grid-cols-1">
-						{courses.map((course) => {
-							const { bgColor, textColor } = getCategoryColors(course.tag);
-							return (
-								<CourseCard
-									key={course.id}
-									imageUrl={course.thumbnail}
-									category={course.tag}
-									categoryBgColor={bgColor}
-									categoryTextColor={textColor}
-									price={`${course.salePrice}`}
-									title={course.title}
-									rating={0.0}
-									students={course.students ?? 0}
-									onClick={() => handleCourseClick(course.id)}
-								/>
-							);
-						})}
-					</div>
-				)}
+				<CourseComponent initialCourses={loading ? null : courses} />
 			</div>
 		</section>
 	);
