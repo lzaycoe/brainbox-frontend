2a34f89db9d8323d37e0e74f337c9e70536c6c09
Thunder <htn10a2@gmail.com>
2025-03-16 19:43:01 +0700

    update: user context for storing info

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,7 +1,19 @@
-export interface ClerkUser {
+interface BankAccount {
+	bank_name: string;
+	account_number: string;
+	account_holder: string;
+}
+
+interface PublicMetadata {
+	role?: 'learner' | 'teacher';
+	bank_account?: BankAccount;
+}
+
+interface ClerkUser {
 	firstName?: string;
 	lastName?: string;
 	imageUrl?: string;
+	publicMetadata?: PublicMetadata;
 	[key: string]: unknown;
 }
 
