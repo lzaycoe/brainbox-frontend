e331f00b06812b55f46ae2d2eebb2cc0c7342d6d
Bun He <hungoffsize@gmail.com>
2025-03-16 18:10:06 +0700

    add: add Category page

---
+++ src/app/(learners)/category/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+
+import ListTeacherByCategory from '@/components/learners/category/ListTeacherByCategory';
+import CourseComponent from '@/components/learners/course-list/Course';
+import ListCourseCard from '@/components/learners/home/ListCourseCard';
+
+const CategoryPage = () => {
+	return (
+		<main className="flex flex-col gap-10 py-10">
+			<div>
+				<ListCourseCard />
+			</div>
+			<div>
+				<h2 className="text-3xl font-semibold text-center mb-8">
+					Top Teachers in Software Engineering
+				</h2>
+				<ListTeacherByCategory />
+			</div>
+			<div>
+				<h2 className="text-3xl font-semibold text-center mb-8">
+					All Courses in Software Engineering
+				</h2>
+				<CourseComponent initialCourses={null} />
+			</div>
+		</main>
+	);
+};
+
+export default CategoryPage;
