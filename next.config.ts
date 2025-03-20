90d8ddba7d9b4ec37b0df56b93d449dc49f57b2d
Thunder <htn10a2@gmail.com>
2025-03-20 15:38:51 +0700

    fix: duplicate issue in next config hostname

--- next.config.ts
+++ next.config.ts
@@ -1,55 +1,17 @@
 import type { NextConfig } from 'next';
 
+import { allowedHostnames } from '@/config/allowedHostnames';
+
+const remotePatterns = allowedHostnames.map((hostname) => ({
+	protocol: 'https' as const,
+	hostname,
+	pathname: '/**',
+}));
+
 const nextConfig: NextConfig = {
 	output: 'standalone',
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'contrib.rocks',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'img.clerk.com',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'jxaztnbdaxoeftmzztcc.supabase.co',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'llrrvktctkrzjjzyguls.supabase.co',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'd3f1iyfxxz8i1e.cloudfront.net',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'encrypted-tbn0.gstatic.com',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'd2ms8rpfqc4h24.cloudfront.net',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'cdn.builder.io',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'api.vietqr.io',
-				pathname: '/**',
-			},
-		],
+		remotePatterns,
 		dangerouslyAllowSVG: true,
 	},
 	async redirects() {
