f791bf70e97653f1515f053bd7f08e864a51698e
Thunder <htn10a2@gmail.com>
2025-03-21 11:19:11 +0700

    update: bank api service

--- src/services/api/bank.ts
+++ src/services/api/bank.ts
@@ -7,7 +7,7 @@ export const getAllBanks = async (): Promise<Bank[]> => {
 		const response = await axios.get('https://api.vietqr.io/v2/banks');
 		return response.data.data;
 	} catch (error) {
-		console.error('Failed to fetch VietQR banks:', error);
-		throw new Error('Failed to fetch VietQR banks');
+		console.error('Failed to fetch banks:', error);
+		throw new Error('Failed to fetch banks');
 	}
 };
