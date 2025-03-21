91e4fca65679473c76668b1b4f71a403dd1f2abd
Thunder <htn10a2@gmail.com>
2025-03-21 11:20:39 +0700

    add: service for get teacher bank account

---
+++ src/services/custom/user/getTeacherBankAccount.ts
@@ -0,0 +1,39 @@
+import { BankAccountData } from '@/schemas/bank.schema';
+import { getUserClerk } from '@/services/api/user';
+
+export const getTeacherBankAccount = async (
+	teacherId: number,
+): Promise<BankAccountData | null> => {
+	try {
+		const teacher = await getUserClerk(teacherId);
+		console.log('teacher', teacher);
+
+		if (!teacher) {
+			console.warn(`Teacher with ID ${teacherId} not found`);
+			return null;
+		}
+
+		const bankAccount = teacher.publicMetadata?.bank_account as
+			| BankAccountData
+			| undefined;
+
+		console.log('bankAccount', bankAccount);
+
+		if (!bankAccount) {
+			console.warn(`Bank account not found for teacher with ID ${teacherId}`);
+			return null;
+		}
+
+		const bankAccountData: BankAccountData = {
+			bank_name: bankAccount.bank_name,
+			account_number: bankAccount.account_number,
+			account_holder: bankAccount.account_holder,
+		};
+		console.log('bankAccountData', bankAccountData);
+
+		return bankAccountData;
+	} catch (error) {
+		console.error('Failed to fetch teacher bank account:', error);
+		throw new Error('Failed to fetch teacher bank account');
+	}
+};
