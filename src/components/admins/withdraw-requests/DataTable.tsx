1eba1225c79669d19c347045dca3e6d9fad17cb6
Thunder <htn10a2@gmail.com>
2025-03-21 11:22:02 +0700

    update: data table component

--- src/components/admins/withdraw-requests/DataTable.tsx
+++ src/components/admins/withdraw-requests/DataTable.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {
-	ColumnDef,
 	ColumnFiltersState,
 	SortingState,
 	VisibilityState,
@@ -12,24 +11,17 @@ import {
 	getSortedRowModel,
 	useReactTable,
 } from '@tanstack/react-table';
-import {
-	ArrowUpDown,
-	ChevronDown,
-	MoreHorizontal,
-	RefreshCw,
-} from 'lucide-react';
 import * as React from 'react';
 import { useEffect } from 'react';
+import { FaChevronDown, FaSyncAlt } from 'react-icons/fa';
 
+import { createColumns } from '@/components/admins/withdraw-requests/Columns';
 import Loading from '@/components/commons/Loading';
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
 	DropdownMenu,
 	DropdownMenuCheckboxItem,
 	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
@@ -42,212 +34,12 @@ import {
 	TableRow,
 } from '@/components/ui/table';
 import { useToast } from '@/hooks/use-toast';
+import { Bank } from '@/schemas/bank.schema';
 import { User } from '@/schemas/user.schema';
-import { WithdrawHistory } from '@/schemas/withdraw.schema';
+import { getAllBanks } from '@/services/api/bank';
 import { getAllClerkUsers } from '@/services/api/user';
-import {
-	getAllWithdrawals,
-	updateWithdrawStatus,
-} from '@/services/api/withdraw';
-import { formatCurrency } from '@/utils/currency';
-import { formatDateWithdraw } from '@/utils/date';
-
-export type WithdrawRequest = WithdrawHistory & {
-	teacherName: string;
-};
-
-const updateWithdrawData = (
-	prevData: WithdrawRequest[],
-	updatedWithdraw: WithdrawRequest,
-): WithdrawRequest[] => {
-	return prevData.map((item) =>
-		item.id === updatedWithdraw.id
-			? { ...item, status: updatedWithdraw.status }
-			: item,
-	);
-};
-
-const handleUpdateStatus = async (
-	withdrawId: number,
-	status: 'approved' | 'rejected',
-	setData: React.Dispatch<React.SetStateAction<WithdrawRequest[]>>,
-	toast: ReturnType<typeof useToast>['toast'],
-) => {
-	try {
-		const updatedWithdrawHistory = await updateWithdrawStatus(
-			withdrawId,
-			status,
-		);
-		const teacher = (await getAllClerkUsers()).find(
-			(user) => user.id === updatedWithdrawHistory.teacherId,
-		);
-		const updatedWithdraw: WithdrawRequest = {
-			...updatedWithdrawHistory,
-			teacherName: teacher?.clerkUser
-				? `${teacher.clerkUser.firstName ?? ''} ${teacher.clerkUser.lastName ?? ''}`.trim()
-				: 'Unknown Teacher',
-		};
-		toast({
-			title: 'Success',
-			description: `Withdraw request ${status}.`,
-			variant: 'success',
-			duration: 3000,
-		});
-
-		setData((prevData) => updateWithdrawData(prevData, updatedWithdraw));
-	} catch (error) {
-		toast({
-			title: 'Error',
-			description: `Failed to ${status === 'approved' ? 'approve' : 'reject'} withdraw request.`,
-			variant: 'destructive',
-			duration: 3000,
-		});
-		console.error(
-			`Error ${status === 'approved' ? 'approving' : 'rejecting'} withdraw:`,
-			error,
-		);
-	}
-};
-
-export const createColumns = (
-	toast: ReturnType<typeof useToast>['toast'],
-	setData: React.Dispatch<React.SetStateAction<WithdrawRequest[]>>,
-): ColumnDef<WithdrawRequest>[] => [
-	{
-		accessorKey: 'teacherName',
-		header: ({ column }) => (
-			<Button
-				variant="ghost"
-				onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-			>
-				Teacher Name
-				<ArrowUpDown className="ml-2 h-4 w-4" />
-			</Button>
-		),
-		cell: ({ row }) => <div>{row.getValue('teacherName')}</div>,
-	},
-	{
-		accessorKey: 'amount',
-		accessorFn: (row) => {
-			const amountStr =
-				typeof row.amount === 'string' ? row.amount : row.amount.toString();
-			const numericValue = parseFloat(amountStr.replace(/[^0-9.]/g, ''));
-			return isNaN(numericValue) ? 0 : numericValue;
-		},
-		header: ({ column }) => (
-			<Button
-				variant="ghost"
-				onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-			>
-				Amount
-				<ArrowUpDown className="ml-2 h-4 w-4" />
-			</Button>
-		),
-		cell: ({ row }) => {
-			const amount = parseFloat(row.getValue('amount') as string);
-			return (
-				<div className="text-left font-medium">{formatCurrency(amount)}</div>
-			);
-		},
-	},
-	{
-		accessorKey: 'reason',
-		header: 'Reason',
-		cell: ({ row }) => <div>{row.getValue('reason') || 'N/A'}</div>,
-	},
-	{
-		accessorKey: 'createAt',
-		header: ({ column }) => (
-			<Button
-				variant="ghost"
-				onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-			>
-				Date
-				<ArrowUpDown className="ml-2 h-4 w-4" />
-			</Button>
-		),
-		cell: ({ row }) => (
-			<div>{formatDateWithdraw(row.getValue('createAt') as string)}</div>
-		),
-	},
-	{
-		accessorKey: 'status',
-		header: ({ column }) => (
-			<Button
-				variant="ghost"
-				onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-			>
-				Status
-				<ArrowUpDown className="ml-2 h-4 w-4" />
-			</Button>
-		),
-		cell: ({ row }) => {
-			const status = row.getValue('status') as string;
-			let variant: 'default' | 'secondary' | 'destructive' | 'outline' =
-				'default';
-			let className = '';
-
-			switch (status.toLowerCase()) {
-				case 'pending':
-					variant = 'secondary';
-					className = 'bg-yellow-100 text-yellow-800';
-					break;
-				case 'approved':
-					variant = 'secondary';
-					className = 'bg-green-100 text-green-800';
-					break;
-				case 'rejected':
-					variant = 'destructive';
-					className = 'bg-red-100 text-red-800';
-					break;
-			}
-
-			return (
-				<Badge variant={variant} className={`rounded-full ${className}`}>
-					{status}
-				</Badge>
-			);
-		},
-	},
-	{
-		id: 'actions',
-		enableHiding: false,
-		cell: ({ row }) => {
-			const withdraw = row.original as WithdrawRequest;
-
-			const handleApprove = () =>
-				handleUpdateStatus(withdraw.id, 'approved', setData, toast);
-			const handleReject = () =>
-				handleUpdateStatus(withdraw.id, 'rejected', setData, toast);
-
-			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-							<span className="sr-only">Open menu</span>
-							<MoreHorizontal className="h-4 w-4" />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>Actions</DropdownMenuLabel>
-						<DropdownMenuItem
-							onClick={handleApprove}
-							disabled={withdraw.status !== 'pending'}
-						>
-							Approve
-						</DropdownMenuItem>
-						<DropdownMenuItem
-							onClick={handleReject}
-							disabled={withdraw.status !== 'pending'}
-						>
-							Reject
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			);
-		},
-	},
-];
+import { getAllWithdrawals } from '@/services/api/withdraw';
+import { WithdrawRequest } from '@/types/withdrawRequest';
 
 export function WithdrawRequestTable() {
 	const [sorting, setSorting] = React.useState<SortingState>([
@@ -264,7 +56,26 @@ export function WithdrawRequestTable() {
 	const [rowSelection, setRowSelection] = React.useState({});
 	const [data, setData] = React.useState<WithdrawRequest[]>([]);
 	const [loading, setLoading] = React.useState(true);
+	const [banks, setBanks] = React.useState<Bank[]>([]);
 	const { toast } = useToast();
+
+	useEffect(() => {
+		const fetchBanks = async () => {
+			try {
+				const banksData = await getAllBanks();
+				setBanks(banksData);
+			} catch (error) {
+				toast({
+					title: 'Error',
+					description: 'Failed to fetch banks.',
+					variant: 'destructive',
+					duration: 3000,
+				});
+				console.error('Error fetching banks:', error);
+			}
+		};
+		fetchBanks();
+	}, [toast]);
 
 	const fetchData = React.useCallback(async () => {
 		setLoading(true);
@@ -304,7 +115,7 @@ export function WithdrawRequestTable() {
 
 	const table = useReactTable({
 		data,
-		columns: createColumns(toast, setData),
+		columns: createColumns(toast, setData, banks),
 		onSortingChange: setSorting,
 		onColumnFiltersChange: setColumnFilters,
 		getCoreRowModel: getCoreRowModel(),
@@ -349,12 +160,13 @@ export function WithdrawRequestTable() {
 					onClick={fetchData}
 					className="ml-2"
 				>
-					<RefreshCw className="h-4 w-4 mr-2" /> Refresh
+					<FaSyncAlt className="h-4 w-4 mr-2" />
+					Refresh
 				</Button>
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="outline" className="ml-auto">
-							Columns <ChevronDown className="ml-2 h-4 w-4" />
+							Columns <FaChevronDown className="ml-2 h-4 w-4" />
 						</Button>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align="end">
