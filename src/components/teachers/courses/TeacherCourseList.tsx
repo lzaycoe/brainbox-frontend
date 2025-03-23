de043f238f6f6d568911d70e41897c1299e1ec66
Minh Vương <34091632+hardingadonis@users.noreply.github.com>
2025-03-23 20:42:14 +0700

    Update TeacherCourseList.tsx

--- src/components/teachers/courses/TeacherCourseList.tsx
+++ src/components/teachers/courses/TeacherCourseList.tsx
@@ -16,7 +16,6 @@ const TeacherCourseList: React.FC = () => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [searchQuery, setSearchQuery] = useState('');
 	const [selectedCategory, setSelectedCategory] = useState('all');
-	const [selectedRating, setSelectedRating] = useState('all');
 	const coursesPerPage = 12;
 	const { user, loading: userLoading } = useUserContext();
 	const [courses, setCourses] = useState<Course[]>([]);
@@ -47,8 +46,6 @@ const TeacherCourseList: React.FC = () => {
 
 		fetchCourses();
 	}, [userLoading, user]);
-
-	console.log('selectedRating:', selectedRating);
 
 	useEffect(() => {
 		const fetchPaymentsForCourses = async () => {
